ee1441a94eef1eaf2698a3cb6df77e801dddfdb1
enderavour <vovaboba1357@gmail.com>
2026-02-19 13:06:06 +0200

    Added test case

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,28 @@ fn main() -> Result<(), Box<dyn Error>>
     let arr: &mut [u32] = &mut [4, 1, 3, 2];
     sort_array(arr, true)?;
 
+    let arr2: &mut [u16] = &mut [4, 3, 6, 8, 7, 5, 2, 1];
+    sort_array(arr2, true)?;
+
     // First assertion
     assert_eq!(*arr, [1, 2, 3, 4]);
+    // Second Assertion
+    assert_ne!(*arr2, [4, 3, 6, 8, 7, 5, 2, 1]);
 
     let seed = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
     unsafe { srand(seed) };
 
     // Filling array with random numbers
-    let mut arr = [0u8; 32];
-    arr.iter_mut().for_each(|n| *n = (unsafe { rand() } % 256) as u8);
+    let mut rand_arr = [0u8; 32];
+    rand_arr.iter_mut().for_each(|n| *n = (unsafe { rand() } % 256) as u8);
     println!("Initial array:");
-    arr.iter().for_each(|n| print!("{} ", *n));
+    rand_arr.iter().for_each(|n| print!("{} ", *n));
 
     // Sort in descending order
-    sort_array(&mut arr, false)?;
+    sort_array(&mut rand_arr, false)?;
 
     println!("Sorted array:");
-    arr.iter().for_each(|n| print!("{} ", *n));
+    rand_arr.iter().for_each(|n| print!("{} ", *n));
     println!();
 
     Ok(())
